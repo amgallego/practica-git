7adf701f798f3288c6ae8ab5fcfc51cf06964b14
amgallego <anagallego1202@gmail.com>
2025-04-30 20:02:07 -0500

    Otros cambios

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,8 @@ public class Main {
        System.out.println("Hola Mundo");
        System.out.println("Hola Mundo");
        System.out.println("Hola Mundo");
+       for (int i = 0; i < 10; i ++){
+           System.out.println(i);
+       }
     }
 }
